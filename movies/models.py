961eb20a5364f9671ef7f3ec60a098e73ed511e9
mzakharow <zaharow@bk.ru>
2020-01-13 21:14:44 +0300

    all models

--- movies/models.py
+++ movies/models.py
@@ -50,4 +50,53 @@ class Movie(models.Model):
     director = models.ManyToManyField(Participant, verbose_name='режиссер', related_name='film_director')
     actors = models.ManyToManyField(Participant, verbose_name='актеры', related_name='film_actor')
     genres = models.ManyToManyField(Genre, verbose_name='жанры')
+    budget = models.PositiveIntegerField('Бюджет', default=0, help_text='в долларах')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=128, unique=True)
+    draft = models.BooleanField('Черновик', default=False)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+
+class RatingStar(models.Model):
+    value = models.SmallIntegerField('Значение', default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'Звезда рейтинга'
+        verbose_name_plural = 'Звезды рейтинга'
+
+
+class Rating(models.Model):
+    ip = models.CharField('IP адрес', max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='зывезда')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='фильм')
+
+    def __str__(self):
+        return f"{self.star} - {self.movie}"
+
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+
+
+class Review(models.Model):
+    email = models.EmailField()
+    name = models.CharField('Имя', max_length=64)
+    text = models.TextField('Отзыв', max_length=3072)
+    parent = models.ForeignKey('self', verbose_name='Родитель', on_delete=models.SET_NULL, blank=True, null=True)
+    movie = models.ForeignKey(Movie, verbose_name='фильм', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.movie}"
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
