853bc5569600524f6ff6c7845b8d5fb19955e6b0
mzakharow <098098789>
2020-01-17 00:18:05 +0300

    django class View

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -68,6 +69,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"movie_slug": self.url})
 
     class Meta:
         verbose_name = 'Фильм'
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView, ListView
 
-from .models import Movie
+from .models import Movie, Genre
 
 
-class MoviesView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+# class MoviesView(View):
+#     def get(self, request):
+#         movies = Movie.objects.all()
+#         return render(request, "movies/movie_list.html", {"movie_list": movies})
 
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+# class MovieDetailView(View):
+#     def get(self, request, movie_slug):
+#         movie = Movie.objects.get(url=movie_slug)
+#         return render(request, "movies/movie.html", {"movie": movie})
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = "url"
+    template_name = "movies/movie_detail.html"
