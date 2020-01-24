60509240d81a3d6dcf3a9fbac42e5b7cb7ea9bc0
mzakharow <098098789>
2020-01-25 00:57:34 +0300

    tamplate tags

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import DetailView, ListView
 
-from .models import Movie, Genre
+from .models import Movie, Genre, Category
 from .forms import ReviewForm
 
 # class MoviesView(View):
@@ -10,10 +10,17 @@ from .forms import ReviewForm
 #         movies = Movie.objects.all()
 #         return render(request, "movies/movie_list.html", {"movie_list": movies})
 
+
 class MoviesView(ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     # if if there is no tamplate name, a template will be displayed <model_name> + 'list'
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     context["categories"] = Category.objects.all()
+    #     return context
+
 
 # class MovieDetailView(View):
 #     def get(self, request, movie_slug):
@@ -26,6 +33,11 @@ class MovieDetailView(DetailView):
     slug_field = "url"
     # if there is no tamplate_name, a template will be displayed <model_name> + '_detail' (here is movie_detail)
     # template_name = "movies/movie.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     context["categories"] = Category.objects.all()
+    #     return context
 
 
 class AddReview(View):
