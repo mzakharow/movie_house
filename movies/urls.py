33e67ba34dc105e4000a6af2878d4b8d753cb34d
mzakharow <zaharow@bk.ru>
2020-04-06 01:49:51 +0300

    filter "Q" (OR)  "," (AND)

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.MoviesView.as_view()),
+    path("filter/", views.FilterMoviesView.as_view(), name='filter'),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name='movie_detail'),
     path("review/<int:pk>/", views.AddReview.as_view(), name='add_review'),
+    path("participant/<slug:slug>/", views.ParticipantView.as_view(), name='participant_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,19 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import DetailView, ListView
 
-from .models import Movie, Genre, Category
+from .models import Movie, Genre, Category, Participant
 from .forms import ReviewForm
+
+
+class GenreYear:
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
 
 # class MoviesView(View):
 #     def get(self, request):
@@ -11,10 +21,10 @@ from .forms import ReviewForm
 #         return render(request, "movies/movie_list.html", {"movie_list": movies})
 
 
-class MoviesView(ListView):
+class MoviesView(GenreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    # if if there is no tamplate name, a template will be displayed <model_name> + 'list'
+    # if there is no tamplate name, a template will be displayed <model_name> + 'list'
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super().get_context_data(*args, **kwargs)
@@ -28,10 +38,10 @@ class MoviesView(ListView):
 #         return render(request, "movies/movie.html", {"movie": movie})
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     model = Movie
     slug_field = "url"
-    # if there is no tamplate_name, a template will be displayed <model_name> + '_detail' (here is movie_detail)
+    # if there is no template_name, a template will be displayed <model_name> + '_detail' (there is movie_detail)
     # template_name = "movies/movie.html"
 
     # def get_context_data(self, *args, **kwargs):
@@ -50,3 +60,18 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ParticipantView(DetailView):
+    model = Participant
+    # template_name = 'movies/participant_detail.html'
+    slug_field = 'url'  # field for search
+
+
+class FilterMoviesView(GenreYear, ListView):
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |    # Q как логическое ИЛИ
+            Q(genres__in=self.request.GET.getlist("genre"))   # , как просто И
+        )
+        return queryset
